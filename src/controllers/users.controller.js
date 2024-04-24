f3f530576f2fa56439968d91c8e0403cd0a8a716
Luciano Perez <jesus-61914@hotmail.com>
2024-04-23 21:59:55 -0600

    Update users.controller.js
    Se generaron más métodos para el controlador

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -7,10 +7,50 @@ class UserController {
   getUsers = async (req, res) => {
     try {
       res.json({
-        message: 'Hola mundo'
+        message: 'Esta ruta entregaría a los usuarios'
       })
     } catch (error) {
       console.log(error)
+    }
+  }
+
+  getUserById = (req, res) => {
+    try {
+      res.json({
+        message: 'Esta ruta entregaría a un usuario'
+      })
+    } catch (error) {
+
+    }
+  }
+
+  createUser = (req, res) => {
+    try {
+      res.json({
+        message: 'Esta ruta crearía un usuario'
+      })
+    } catch (error) {
+
+    }
+  }
+
+  updateUser = (req, res) => {
+    try {
+      res.json({
+        message: 'Esta ruta actualizaría un usuario'
+      })
+    } catch (error) {
+
+    }
+  }
+
+  deleteUser = (req, res) => {
+    try {
+      res.json({
+        message: 'Esta ruta eliminaría un usuario'
+      })
+    } catch (error) {
+
     }
   }
 }
